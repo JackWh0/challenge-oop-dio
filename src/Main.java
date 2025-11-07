585d1063e4de1460a199a233a52befb022c37abd
Jackson Silva <jackson.silva.who@gmail.com>
2025-11-07 11:12:47 -0300

    add Course and Mentoring classes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,29 @@
+import br.com.dio.challenge.domain.Course;
+import br.com.dio.challenge.domain.Mentoring;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        Course javaCourse = new Course();
+        javaCourse.setTitle("Java course");
+        javaCourse.setDescription("OOP Course! Click right now!");
+        javaCourse.setWorkload(40);
+        System.out.println(javaCourse.toString());
 
+        Course pythonCourse = new Course();
+        pythonCourse.setTitle("Python course");
+        pythonCourse.setDescription("Python Course! Already available!");
+        pythonCourse.setWorkload(25);
+        System.out.println(pythonCourse.toString());
+
+        Mentoring javaMentoring = new Mentoring();
+        javaMentoring.setTitle("Java mentoring");
+        javaMentoring.setDescription("Watch this mentoring to get more knowledge");
+        javaMentoring.setDate(LocalDate.now());
+        System.out.println(javaMentoring.toString());
 
     }
 }
